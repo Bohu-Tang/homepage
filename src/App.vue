de35216968374d07c7b36a4f68ac8730af68ae46
肖鲁新 <losen_xiao@skytech.cn>
2022-12-01 13:54:28 +0800

    备案号

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import appHeader from '@/components/appHeader.vue'
 import baseLayout from '@/layout/baseLayout.vue'
+import AppFooter from "./components/appFooter.vue";
 </script>
 
 <template>
   <appHeader></appHeader>
   <baseLayout></baseLayout>
+  <app-footer></app-footer>
 </template>
 
 
---
+++ src/components/appFooter.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <div class="app-footer">
+    鲁ICP备19013167号-1
+  </div>
+</template>
+<style lang="less">
+.app-footer{
+  height: 30px;
+  text-align: center;
+  background-color: rgba(255, 255, 255, 0.8);
+  box-shadow: 0 4px 10px #fff;
+}
+</style>
